77715d29205424234c12fa12557a3f5b5d01d57e
Gihong Kim <gyong1211@gmail.com>
2017-07-25 21:43:17 +0900

    add static, media file path in config.urls

--- django_app/config/settings/base.py
+++ django_app/config/settings/base.py
@@ -103,6 +103,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     STATIC_DIR,
 ]
