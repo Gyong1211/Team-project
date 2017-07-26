e265f85fedda236300b88ade8dff7f39d9fadcc4
Gihong Kim <gyong1211@gmail.com>
2017-07-26 20:18:23 +0900

    config urls 패키지 삭제

--- django_app/config/urls/__init__.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url, include
-from config.urls import urls_apis, urls_views
-
-urlpatterns = [
-    url(r'^api/', include(urls_apis)),
-    url(r'', include(urls_views)),
-]
