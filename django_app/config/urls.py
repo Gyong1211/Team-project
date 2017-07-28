306bc025cfe3d67dba2d25bec2457c4fae73d481
Gihong Kim <gyong1211@gmail.com>
2017-07-28 15:28:49 +0900

    AWS 배포환경에 필요없는 static url patterns 주석처리

--- django_app/config/urls.py
+++ django_app/config/urls.py
@@ -21,5 +21,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
