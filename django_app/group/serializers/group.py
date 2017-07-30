9432884c885a256ea2c894b93a278383e0bf963e
Gihong Kim <gyong1211@gmail.com>
2017-07-31 00:37:02 +0900

    GroupListSerializer 필드 속성 변경 및 create의 경우, request.user를 받아 owner로 지정하도록 method override

--- django_app/group/serializers/group.py
+++ django_app/group/serializers/group.py
@@ -6,9 +6,10 @@ from ..models import MyGroup
 
 
 class GroupListSerializer(serializers.ModelSerializer):
-    owner = UserSerializer(many=False)
-    profile_img_url = serializers.SerializerMethodField()
+    owner = UserSerializer(many=False, read_only=True)
+    profile_img_url = serializers.SerializerMethodField(read_only=True)
     tags = TagSerializer(many=True, read_only=True, allow_null=True)
+    tag = serializers.CharField(max_length=255, allow_blank=True, write_only=True)
 
     class Meta:
         model = MyGroup
@@ -21,10 +22,17 @@ class GroupListSerializer(serializers.ModelSerializer):
             'description',
             'num_of_members',
             'tags',
+            'tag',
+        )
+        read_only_fields = (
+            'num_of_members',
         )
 
     def get_profile_img_url(self, obj):
         return obj.profile_img.url
+
+    def create(self, validated_data):
+        return MyGroup.objects.create(**validated_data, owner=self.context['request'].user)
 
 
 class GroupDetailSerializer(serializers.ModelSerializer):
