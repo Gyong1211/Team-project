d744242494e8451f8a5828a23ab4845ae41c2d87
Gihong Kim <gyong1211@gmail.com>
2017-07-30 18:52:59 +0900

    Group의 List 검색 방식 변경(띄워쓰기로 구분 된 여러개의 검색어를 적용할 수 있도록 변경)

--- django_app/group/apis/group.py
+++ django_app/group/apis/group.py
@@ -10,16 +10,23 @@ class GroupListCreateView(generics.ListCreateAPIView):
     serializer_class = GroupListSerializer
 
     def get_queryset(self):
-        keyword = self.request.GET.get('search', '')
-        if keyword:
+        keyword_list = self.request.GET.get('search', '')
+        if keyword_list:
             # 멤버 숫자를 Integer 값으로 저장하는 필드 생성 후, 검색 출력 결과를 멤버수로 정렬하도록 .order_by() 추가 필요
             if self.request.user.is_staff:
-                return MyGroup.objects.filter(Q(name__contains=keyword) | Q(tags__name__contains=keyword))
+                result_queryset = MyGroup.objects.all()
+                for keyword in keyword_list.split():
+                    result_queryset = result_queryset.filter(
+                        Q(name__contains=keyword) | Q(description__contains=keyword) | Q(tags__name__contains=keyword)
+                    )
+                return result_queryset
             else:
-                return MyGroup.objects.exclude(group_type="HIDDEN").filter(
-                    Q(name__contains=keyword) | Q(tags__name__contains=keyword)
-                )
-
+                result_queryset = MyGroup.objects.exclude(group_type="HIDDEN")
+                for keyword in keyword_list.split():
+                    result_queryset = result_queryset.filter(
+                        Q(name__contains=keyword) | Q(description__contains=keyword) | Q(tags__name__contains=keyword)
+                    )
+                return result_queryset
         else:
             if self.request.user.is_staff:
                 return MyGroup.objects.all()
