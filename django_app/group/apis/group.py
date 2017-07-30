92a4dfb6ecc84ae5e08ae943d13c298a8ece7010
Gihong Kim <gyong1211@gmail.com>
2017-07-30 18:25:14 +0900

    GroupListCreateView queryset 세분화(Hidden 그룹은 관리자만 검색 가능, search parameter를 이용해, 그룹의 name 또는 tag name에 검색어가 포함 된 그룹만 출력하도록 변경)

--- django_app/group/apis/group.py
+++ django_app/group/apis/group.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from rest_framework import generics
 
 from member.serializers import UserSerializer
@@ -6,8 +7,24 @@ from ..models import MyGroup
 
 
 class GroupListCreateView(generics.ListCreateAPIView):
-    queryset = MyGroup.objects.all()
     serializer_class = GroupSerializer
+
+    def get_queryset(self):
+        keyword = self.request.GET.get('search', '')
+        if keyword:
+            # 멤버 숫자를 Integer 값으로 저장하는 필드 생성 후, 검색 출력 결과를 멤버수로 정렬하도록 .order_by() 추가 필요
+            if self.request.user.is_staff:
+                return MyGroup.objects.filter(Q(name__contains=keyword) | Q(tags__name__contains=keyword))
+            else:
+                return MyGroup.objects.exclude(group_type="HIDDEN").filter(
+                    Q(name__contains=keyword) | Q(tags__name__contains=keyword)
+                )
+
+        else:
+            if self.request.user.is_staff:
+                return MyGroup.objects.all()
+            else:
+                return MyGroup.objects.exclude(group_type="HIDDEN")
 
 
 class GroupRetrieveView(generics.RetrieveAPIView):
