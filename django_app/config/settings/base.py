0d171422f86c38ceacba4e52fa7ffab43cc1551e
ym4199 <ym4199@gmail.com>
2017-08-01 01:12:25 +0900

    pagination 통해 나타낼 수 있는 수 조절

--- django_app/config/settings/base.py
+++ django_app/config/settings/base.py
@@ -110,3 +110,9 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS':
+    'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
+}
