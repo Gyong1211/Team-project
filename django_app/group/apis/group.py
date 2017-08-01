18d6a440e0927376e63497499b3b1fdd09255d14
Gihong Kim <gyong1211@gmail.com>
2017-08-01 15:17:59 +0900

    get_queryset에 queryset.filter를 이용해 구현한 검색 기능을 rest_framework의 filters를 사용하도록 변경

--- django_app/group/apis/group.py
+++ django_app/group/apis/group.py
@@ -1,36 +1,19 @@
-from django.db.models import Q
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 
-from member.serializers import UserSerializer
-from post.serializers import PostSerializer
 from utils.permissions import ObjectOwnerIsRequestUser
 from ..serializers import GroupSerializer, GroupCreateSerializer, GroupUpdateSerializer
 from ..models import MyGroup
 
 
 class GroupListCreateView(generics.ListCreateAPIView):
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'description', 'tags__name')
+
     def get_queryset(self):
-        keyword_list = self.request.GET.get('search', '')
-        if keyword_list:
-            if self.request.user.is_staff:
-                result_queryset = MyGroup.objects.all()
-                for keyword in keyword_list.split():
-                    result_queryset = result_queryset.filter(
-                        Q(name__contains=keyword) | Q(description__contains=keyword) | Q(tags__name__contains=keyword)
-                    ).order_by('-num_of_members')
-                return result_queryset
-            else:
-                result_queryset = MyGroup.objects.exclude(group_type="HIDDEN")
-                for keyword in keyword_list.split():
-                    result_queryset = result_queryset.filter(
-                        Q(name__contains=keyword) | Q(description__contains=keyword) | Q(tags__name__contains=keyword)
-                    ).order_by('-num_of_members')
-                return result_queryset
+        if self.request.user.is_staff:
+            return MyGroup.objects.all()
         else:
-            if self.request.user.is_staff:
-                return MyGroup.objects.all()
-            else:
-                return MyGroup.objects.exclude(group_type="HIDDEN")
+            return MyGroup.objects.exclued(group_type="HIDDEN")
 
     def get_serializer_class(self):
         if self.request.method == 'GET':
