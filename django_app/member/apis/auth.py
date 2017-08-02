87af4016d692918f7ee1e9d858ae1dd63ad07ba5
ym4199 <ym4199@gmail.com>
2017-08-03 01:18:23 +0900

    loginview/logoutview 토큰값 포함하여 구현

--- django_app/member/apis/auth.py
+++ django_app/member/apis/auth.py
@@ -1,28 +1,35 @@
+from django.utils import timezone
 from rest_framework import parsers, renderers
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from ..serializers import LoginSerializer
+from ..serializers import AuthTokenSerializer
 
 __all__ = (
-    'AuthTokenView',
+    'LoginView',
+    'LogoutView',
 )
 
 
-class AuthTokenView(APIView):
+class LoginView(APIView):
     throttle_classes = ()
-    permissions_classes = ()
+    permission_classes = ()
     parser_classes = (parsers.FormParser, parsers.MultiPartParser, parsers.JSONParser,)
     renderer_classes = (renderers.JSONRenderer,)
-    serializer_class = LoginSerializer
+    serializer_class = AuthTokenSerializer
 
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({'token': token.key})
+        email = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=email)
+        email.last_login = timezone.now()
+        email.save(update_fields=['last_login'])
+        return Response('Login complete')
 
 
-auth_token = AuthTokenView.as_view()
+class LogoutView(APIView):
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response('Logout complete')
