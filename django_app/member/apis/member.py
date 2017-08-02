b31afa18c300455c7c59a44289204100f8cc1aa0
ym4199 <ym4199@gmail.com>
2017-08-03 01:19:41 +0900

    updateview 의 patch 구현

--- django_app/member/apis/member.py
+++ django_app/member/apis/member.py
@@ -1,12 +1,9 @@
-from django.utils.datastructures import MultiValueDictKeyError
-from rest_framework import generics, status
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics, permissions
 
 from member.serializers import UserUpdateSerializer
+from utils.permissions import ObjectIsRequestUser
 from ..models import MyUser
-from ..serializers import UserSerializer
+from ..serializers import UserSerializer, UserCreateSerializer
 
 
 class UserListCreateView(generics.ListCreateAPIView):
@@ -16,43 +13,17 @@ class UserListCreateView(generics.ListCreateAPIView):
         if self.request.method == 'GET':
             return UserSerializer
         elif self.request.method == 'POST':
-            return UserSerializer
-
-#
-# class UserProfileView(APIView):
-#     def patch(self, request, *args, **kwargs):
-#         user = request.user
-#         try:
-#             for request_item in request.data.keys():
-#                 if request_item not in [item for item in UserSerializer(user).fields]:
-#                     return Response(status=status.HTTP_400_BAD_REQUEST, data={"detail": "data 타입이 잘못되었습니다"})
-#             nickname = request.data.get("nickname", user.nickname)
-#             if MyUser.objects.filter(nickname=nickname).exists():
-#                 return Response(status=status.HTTP_400_BAD_REQUEST, data={"detail": "해당 nickname 을 다른 유저가 사용중입니다"})
-#             else:
-#                 # UserSerializer(user).data['nickname'] = user.nickname[args]
-#                 return Response(status=status.HTTP_200_OK, data=UserSerializer(user).data)
-#         except MultiValueDictKeyError:
-#             return Response(status=status.HTTP_400_BAD_REQUEST, data={"detail": "잘못된 형식의 data 입니다"})
-#
-#     def delete(self, request, format=None):
-#         user = request.user
-#         # user 가 자신의 profile 로 들어가서 삭제버튼을 눌러야만 삭제할 수 있다.
-#         user.delete()
-#         ret = {
-#             "detail": "유저가 삭제되었습니다."
-#         }
-#         return Response(ret)
+            return UserCreateSerializer
 
 
 class UserUpdateView(generics.RetrieveUpdateDestroyAPIView):
     queryset = MyUser.objects.all()
     serializer_class = UserUpdateSerializer
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        ObjectIsRequestUser,
+    )
 
-    # def get_serializer_class(self):
-    #     if self.request.method == 'GET':
-    #         return UserSerializer
-    #     elif self.request.method == 'POST':
-    #         return UserCreationSerializer
-    #     elif self.request.method == 'PATCH':
-    #         return UserUpdateSerializer
+    def get_serializer_class(self):
+        if self.request.method == 'PATCH':
+            return UserUpdateSerializer
