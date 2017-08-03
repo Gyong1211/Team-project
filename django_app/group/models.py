6e3b949c192556b1f3cb51aa02b2a54ec3901ff0
Gihong Kim <gyong1211@gmail.com>
2017-08-03 15:27:17 +0900

    MyGroup에서 사용하던 Membership 중간자 모델을 MyUser가 사용하도록 변경하기 위해 필드 및 모델 삭제 후 migration 파일 생성, migrate

--- django_app/group/models.py
+++ django_app/group/models.py
@@ -1,8 +1,6 @@
 from django.conf import settings
 from django.db import models
 from django.db.models.query import QuerySet
-from django.db.models.signals import post_save, post_delete
-from django.dispatch import receiver
 
 from utils.fields import CustomImageField
 
@@ -12,7 +10,7 @@ class MyGroupManager(models.Manager):
         obj = self.model(**kwargs)
         self._for_write = True
         obj.save(force_insert=True, using=self.db)
-        Membership.objects.get_or_create(user=obj.owner, group=obj)
+        # Membership.objects.get_or_create(user=obj.owner, group=obj)
         return obj
 
 
@@ -41,11 +39,6 @@ class MyGroup(models.Model):
         default_static_image='images/no_image.png'
     )
     description = models.CharField(max_length=120, blank=True, null=True)
-    member = models.ManyToManyField(
-        settings.AUTH_USER_MODEL,
-        through='Membership',
-        related_name='groups_joined',
-    )
     num_of_members = models.PositiveIntegerField(default=1)
     tags = models.ManyToManyField(
         'GroupTag',
@@ -93,31 +86,8 @@ class MyGroup(models.Model):
             raise ValueError('잘못된 값을 입력했습니다.')
 
 
-class Membership(models.Model):
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
-    )
-    group = models.ForeignKey(
-        MyGroup,
-        on_delete=models.CASCADE
-    )
-    joined_date = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        unique_together = (
-            ('user', 'group')
-        )
-
-
 class GroupTag(models.Model):
     name = models.CharField(max_length=128, unique=True)
 
     def __str__(self):
         return self.name
-
-
-@receiver(post_save, sender=Membership)
-@receiver(post_delete, sender=Membership)
-def update_num_of_members(sender, instance, **kwargs):
-    instance.group.calc_num_of_members()
