4fcd0a7e140aa5d144e40fe99a385af57ca02c86
ym4199 <ym4199@gmail.com>
2017-08-04 16:53:55 +0900

    userrelation api urls 연결

--- django_app/member/urls.py
+++ django_app/member/urls.py
@@ -4,9 +4,8 @@ from . import apis
 
 urlpatterns = [
     url(r'^(?P<pk>\d+)/', apis.UserUpdateView.as_view()),
-
-
-    url(r'^login/', apis.LoginView.as_view()),
-    url(r'^logout/', apis.LogoutView.as_view()),
+    url(r'^login/$', apis.LoginView.as_view()),
+    url(r'^logout/$', apis.LogoutView.as_view()),
     url(r'^$', apis.UserListCreateView.as_view()),
+    url(r'^relation/$', apis.UserRelationView.as_view()),
 ]
