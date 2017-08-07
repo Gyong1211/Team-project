ca6be54e9eee3238167304734ae754abb8d601da
DahamLee <daham1224@gmail.com>
2017-08-07 19:17:42 +0900

    comment create view 생성, 해당 post pk값을 받아서 생성할 수 있도록 함

--- django_app/post/apis/comment.py
+++ django_app/post/apis/comment.py
@@ -2,31 +2,36 @@ from django.db.models import Q
 from rest_framework import generics, permissions
 
 from utils.permissions import ObjectAuthorIsRequestUser
-from ..models import Comment
+from ..models import Comment, Post
 from ..serializers import CommentSerializer
 
 __all__ = (
-    'CommentListCreateView',
+    'CommentCreateView',
     'CommentRetrieveUpdateDestroyView',
-
 )
 
 
-class CommentListCreateView(generics.ListCreateAPIView):
+class CommentCreateView(generics.CreateAPIView):
+    # def get_queryset(self):
+    #     if 'pk' in self.kwargs:
+    #         pk = self.kwargs['pk']
+    #         Comment.objects.filter(post_set__pk=pk)
+    #     else:
+    #         None
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+        post_pk = self.kwargs['pk']
+        serializer.save(author=self.request.user, post=Post.objects.get(pk=post_pk))
 
 
 class CommentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    def get_queryset(self):
-        if self.request.user.is_authenticated:
-            return Comment.objects.filter(author=self.request.user)
-
+    # def get_queryset(self):
+    #     if self.request.user.is_authenticated:
+    #         return Comment.objects.filter(author=self.request.user)
+    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-
     permission_classes = (
         permissions.IsAuthenticatedOrReadOnly,
         ObjectAuthorIsRequestUser,
--- django_app/post/urls.py
+++ django_app/post/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^mygroup/$', apis.MyGroupPostListView.as_view()),
     url(r'^$', apis.PostListCreateView.as_view()),
     url(r'^(?P<pk>\d+)/$', apis.PostRetrieveUpdateDestroyView.as_view()),
+    url(r'^(?P<pk>\d+)/comment/$', apis.CommentCreateView.as_view()),
+    url(r'^comment/(?P<pk>\d+)/$', apis.CommentRetrieveUpdateDestroyView.as_view()),
 ]
