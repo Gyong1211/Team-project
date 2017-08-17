e102f754e6fe160d24cc099000310374cb365719
ym4199 <ym4199@gmail.com>
2017-08-17 15:20:08 +0900

    user relation test

--- django_app/member/tests.py
+++ django_app/member/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from member.models import MyUser
+from member.models import MyUser, UserRelation
 
 
 class UserModelTest(TestCase):
@@ -11,3 +11,13 @@ class UserModelTest(TestCase):
 
         self.assertEqual(user, user_match)
 
+        def test_userrelation(self):
+            user1 = MyUser.objects.create(email='test1@test.com', nickname='test1')
+            user2 = MyUser.objects.create(email='test2@test.com', nickname='test2')
+
+            relation = UserRelation.objects.create(user1=user1, user2=user2)
+            relation_m = UserRelation.objects.first()
+
+            self.assertEqual(relation, relation_m)
+            self.assertEqual(user1, relation.user1)
+            self.assertEqual(user2, relation.user2)
