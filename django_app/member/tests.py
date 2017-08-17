5d742d60d9c44d63555a55662e86f1ddcd75a323
ym4199 <ym4199@gmail.com>
2017-08-17 15:19:23 +0900

    user create test

--- django_app/member/tests.py
+++ django_app/member/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from member.models import MyUser
+
+
+class UserModelTest(TestCase):
+    def test_user_create(self):
+        user = MyUser.objects.create(email='test@test.com', nickname='test')
+
+        user_match = MyUser.objects.first()
+
+        self.assertEqual(user, user_match)
+
